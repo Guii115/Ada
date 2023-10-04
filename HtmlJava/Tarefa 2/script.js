d871ddf8298394abbf6637a7798c228f83c2d46f
Guii <guilhermebernardino1000@gmail.com>
2023-10-04 20:22:24 -0300

    ...

--- HtmlJava/Tarefa 2/script.js
+++ HtmlJava/Tarefa 2/script.js
@@ -5,6 +5,13 @@ function validarFormulario() {
     var senha = document.getElementById("senha").value;
     var cidade = document.getElementById("cidade").value;
 
+    var dadosRecebidos = document.getElementById("dadosRecebidos");
+    dadosRecebidos.innerHTML = "Nome: " + nome + "<br>" +
+        "Email: " + email + "<br>" +
+        "Idade: " + idade + "<br>" +
+        "Senha: " + senha + "<br>" +
+        "Cidade: " + cidade;
+
     var mensagemErro = document.getElementById("mensagemErro");
     mensagemErro.innerHTML = "";
 
